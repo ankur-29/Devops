c8392334c8c4257bfb30965b20afa0ab5ad17a68
Ankur <ankur.anand.me044@gmail.com>
2025-08-20 22:47:39 +0530

    python-basics

---
+++ python_basics/dataType.py
@@ -0,0 +1,11 @@
+a = 10
+print(a)
+
+b = "This is some string"
+print(b)
+
+c = 3.14
+d = False
+
+print(c)
+print(d)
---
+++ python_basics/object_basic.py
@@ -0,0 +1,25 @@
+# this is a list
+a = [1,2,3,4,5,"String", True, 5.76]
+print(a)
+
+#this is a list having only number
+int_list = [1,2,3,4,5,6,7,8,9,10]
+print(int_list[2])
+
+result = int_list[1:5]
+print(result)
+
+int_list.append(25)
+print(int_list)
+
+# tuple example
+tuple_list = (1, 2, 3, 4, 5)
+print(tuple_list)
+
+#set example
+set_list = {1, 2, 3, 4, 5}
+print(set_list)
+
+#dictionary example
+dic_list = {'a':1, 'b':2, 'c':3}
+print(dic_list)
